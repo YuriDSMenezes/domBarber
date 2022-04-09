61f13854c4a4d880697adb909ab943d416872354
Edgar Ribeiro <eneto774@gmail.com>
2022-04-09 16:29:22 -0300

    Add MainLayout and Add Eslint COnfig

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const MainLayout: React.FC = ({ children, ...rest }) => (
+    <Container {...rest}>
+      {children}
+    </Container>
+);
+
+export default MainLayout;
---
+++ src/pages/appointments/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { NextPage } from 'next';
+import * as S from './styles';
+
+const appointments: NextPage = () => (
+  <S.Container>
+    Teste Appointments
+  </S.Container>
+);
+
+export default appointments;
