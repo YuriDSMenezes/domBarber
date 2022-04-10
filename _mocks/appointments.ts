8d4e64ff5c0541bc777b69857fef776a8afcec37
Edgar Ribeiro <eneto774@gmail.com>
2022-04-09 21:36:08 -0300

    Appointment Page

---
+++ _mocks/appointments.ts
@@ -0,0 +1,42 @@
+export const appointments = [
+  {
+    date: new Date(),
+    description: 'Corte Simples',
+    value: 19.9,
+    points: 20,
+    paymentStatus: 'Pagamento Pendente',
+    professional: 'Paulo R.',
+    avatar:
+      'https://images.pexels.com/photos/1516680/pexels-photo-1516680.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+  },
+  {
+    date: new Date(),
+    description: 'Corte Simples',
+    value: 19.9,
+    points: 20,
+    paymentStatus: 'Pagamento Pendente',
+    professional: 'Paulo R.',
+    avatar:
+      'https://images.pexels.com/photos/1516680/pexels-photo-1516680.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+  },
+  {
+    date: new Date(),
+    description: 'Corte Simples',
+    value: 19.9,
+    points: 20,
+    paymentStatus: 'Pagamento Pendente',
+    professional: 'Paulo R.',
+    avatar:
+      'https://images.pexels.com/photos/1516680/pexels-photo-1516680.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+  },
+  {
+    date: new Date(),
+    description: 'Corte Simples',
+    value: 19.9,
+    points: 20,
+    paymentStatus: 'Pagamento Pendente',
+    professional: 'Paulo R.',
+    avatar:
+      'https://images.pexels.com/photos/1516680/pexels-photo-1516680.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+  },
+];
--- src/pages/appointments/styles.ts
+++ src/pages/appointments/styles.ts
@@ -1,5 +1,23 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const Content = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
+export const Title = styled.h1`
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 21px;
+  color: #ffffff;
+  margin-bottom: 20px;
+`;
+
+export const AppointmentsContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
 `;
