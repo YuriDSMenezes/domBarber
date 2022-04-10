98a6dd085d587736f8383df4d65506fbdfa64d23
Edgar Ribeiro <eneto774@gmail.com>
2022-04-10 10:13:21 -0300

    Fix Heigth in BottomSheetFixedLayout

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,39 +1,42 @@
-import * as S from './styles'
-import Image from 'next/image'
-import { MenuController } from './menu.controller'
+import Image from 'next/image';
+import { LogoWhiteIcon, MenuIcon, NotificationIcon } from 'assets';
+import * as S from './styles';
+import { MenuController } from './menu.controller';
 
-import { LogoWhiteIcon, MenuIcon, NotificationIcon } from 'assets'
-import { Sidebar } from './Sidebar'
+import { Sidebar } from './Sidebar';
 
 export const Menu = () => {
-
   const {
     actions: { handleClickSidebar },
-    state:{ openSidebar , menuItemsList}
-    } = MenuController()
+    state: { openSidebar, menuItemsList },
+  } = MenuController();
 
   return (
     <>
-      {openSidebar && <Sidebar openSidebar={openSidebar} handleClickSidebar={handleClickSidebar} menuItemsList={menuItemsList} />}
+      {openSidebar && (
+        <Sidebar
+          openSidebar={openSidebar}
+          handleClickSidebar={handleClickSidebar}
+          menuItemsList={menuItemsList}
+        />
+      )}
       <S.Container openSidebar={openSidebar}>
         <S.UserInfos>
-          <S.ImageUser>
-            Foto
-          </S.ImageUser>
+          <S.ImageUser>Foto</S.ImageUser>
 
           <S.Notifications hasNotification>
-            <Image src={NotificationIcon} alt='Notificações' />
+            <Image src={NotificationIcon} alt="Notificações" />
           </S.Notifications>
         </S.UserInfos>
 
         <S.Logo>
-          <Image src={LogoWhiteIcon} alt='Logo da Dom Barber' />
+          <Image src={LogoWhiteIcon} alt="Logo da Dom Barber" />
         </S.Logo>
 
         <S.Sidebar onClick={handleClickSidebar}>
-          <Image src={MenuIcon} alt='Menu' />
+          <Image src={MenuIcon} alt="Menu" />
         </S.Sidebar>
       </S.Container>
-  </>
-  )
-}
+    </>
+  );
+};
--- src/pages/branchsoffice/index.tsx
+++ src/pages/branchsoffice/index.tsx
@@ -2,27 +2,30 @@ import React from 'react';
 import BottomSheetFixedLayout from 'layouts/BottomSheetFixedLayout';
 
 import Input from 'components/Input';
+import MainLayout from 'layouts/MainLayout';
 import * as S from './styles';
 import BranchOfficeCard from './BranchOfficeCard';
 
 const branchsoffice: React.FC = () => {
   return (
-    <BottomSheetFixedLayout theme="dark">
-      <S.Content>
-        <S.Title>Filiais</S.Title>
-        <S.SubTitle>Conheça nossas Filiais</S.SubTitle>
-        <Input search secondary placeholder="Pesquisar local" />
-        <S.BranchsOfficeListContainer>
-          <BranchOfficeCard />
-          <BranchOfficeCard />
-          <BranchOfficeCard />
-          <BranchOfficeCard />
-          <BranchOfficeCard />
-          <BranchOfficeCard />
-          <BranchOfficeCard />
-        </S.BranchsOfficeListContainer>
-      </S.Content>
-    </BottomSheetFixedLayout>
+    <MainLayout>
+      <BottomSheetFixedLayout theme="dark">
+        <S.Content>
+          <S.Title>Filiais</S.Title>
+          <S.SubTitle>Conheça nossas Filiais</S.SubTitle>
+          <Input search secondary placeholder="Pesquisar local" />
+          <S.BranchsOfficeListContainer>
+            <BranchOfficeCard />
+            <BranchOfficeCard />
+            <BranchOfficeCard />
+            <BranchOfficeCard />
+            <BranchOfficeCard />
+            <BranchOfficeCard />
+            <BranchOfficeCard />
+          </S.BranchsOfficeListContainer>
+        </S.Content>
+      </BottomSheetFixedLayout>
+    </MainLayout>
   );
 };
 
