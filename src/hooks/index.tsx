7b7b0cb4f1fbc5a40ff89917ba782066fb7980f4
Yuri <yuri.menezes_dextra@prestador.globo>
2022-06-27 13:05:41 -0300

    change loading hook

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,5 +1,5 @@
 export { useCart, CartProvider } from './useCart';
 export { setTheme } from './theme';
 export { ManifestConfig } from './manifestConfig';
-export { useLoading, LoadingProvider } from './Loading';
+export { useLoading, LoadingProvider } from './loading';
 export { useGlobal, GlobalProvider } from './global';
